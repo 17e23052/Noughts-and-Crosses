2c985654e13d4eccbe09cdf8222a62aa37e1627a
Mathman <70949660+17e23052@users.noreply.github.com>
2021-06-14 11:09:34 +0000

    won code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 board = [[" ", " ", " "],
          [" ", " ", " "],
          [" ", " ", " "]]
+player = "X"
 def displayboard(board):
     print(" ", board[0][0], "│", board[0][1], "│", board[0][2])
     print(" ───┼───┼───")
@@ -8,3 +9,23 @@ def displayboard(board):
     print(" ───┼───┼───")
     print(" ", board[2][0], "│", board[2][1], "│", board[2][2])
 displayboard(board)
+def check_win(board, player):
+    won = False
+    if board[0][0] == player and board[0][1] == player and board[0][2] == player:
+        won = True
+    elif board[1][0] == player and board[1][1] == player and board[1][2] == player:
+        won = True
+    elif board[2][0] == player and board[2][1] == player and board[2][2] == player:
+        won = True
+    elif board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        won = True
+    elif board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        won = True
+    elif board[0][0] == player and board[1][0] == player and board[2][0] == player:
+        won = True
+    elif board[0][1] == player and board[1][1] == player and board[2][1] == player:
+        won = True
+    elif board[0][2] == player and board[1][2] == player and board[2][2] == player:
+        won = True
+    print(f"Won = {won}")
+check_win(board, player)
