2ce08e8b59d23530b183c113945a279666699791
Mathman <70949660+17e23052@users.noreply.github.com>
2021-06-15 09:47:48 +0000

    instructions

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
+from time import sleep
 board = [[" ", " ", " "],
          [" ", " ", " "],
          [" ", " ", " "]]
-player = "X"
+def instructions(board):
+  print("Welcome to Noughts and Crosses!")
+  sleep(2)
+  print("Player 1 will be noughts, and player 2 will be crosses.")
+  sleep(2)
+  print("Enter the number 1 to 9 to choose a location on the grid")
+  sleep(2)
+  print("To place your O or X on the board!")
+  sleep(2)
+instructions(board)
 def displayboard(board):
     print(" ", board[0][0], "│", board[0][1], "│", board[0][2])
     print(" ───┼───┼───")
@@ -9,6 +19,7 @@ def displayboard(board):
     print(" ───┼───┼───")
     print(" ", board[2][0], "│", board[2][1], "│", board[2][2])
 displayboard(board)
+player = "X"
 def check_win(board, player):
     won = False
     if board[0][0] == player and board[0][1] == player and board[0][2] == player:
@@ -27,5 +38,4 @@ def check_win(board, player):
         won = True
     elif board[0][2] == player and board[1][2] == player and board[2][2] == player:
         won = True
-    print(f"Won = {won}")
 check_win(board, player)
