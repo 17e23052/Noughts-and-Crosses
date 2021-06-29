abafb34bdc0bf0a495204f539d320672635c7cda
Mathman <70949660+17e23052@users.noreply.github.com>
2021-06-29 10:15:14 +0000

    new 29/6

--- main.py
+++ main.py
@@ -20,15 +20,34 @@ def instructions(board):
    
    """)
   sleep(2)
-instructions(board)
 def displayboard(board):
     print(" ", board[0][0], "│", board[0][1], "│", board[0][2])
     print(" ───┼───┼───")
     print(" ", board[1][0], "│", board[1][1], "│", board[1][2])
     print(" ───┼───┼───")
     print(" ", board[2][0], "│", board[2][1], "│", board[2][2])
-displayboard(board)
 player = "X"
+def move(board, player):
+  print("Where do you want to place your cross?")
+  position = int(input())
+  if position == "1":
+    board[0][0] = player
+  elif position == "2":
+    board[0][1] = player
+  elif position == "3":
+    board[0][2] = player
+  elif position == "4":
+    board[1][0] = player
+  elif position == "5":
+    board[1][1] = player
+  elif position == "6":
+    board[1][2] = player
+  elif position == "7":
+    board[2][0] = player
+  elif position == "8":
+    board[2][1] = player
+  elif position == "9":
+    board[2][2] = player
 def check_win(board, player):
     won = False
     if board[0][0] == player and board[0][1] == player and board[0][2] == player:
@@ -47,4 +66,11 @@ def check_win(board, player):
         won = True
     elif board[0][2] == player and board[1][2] == player and board[2][2] == player:
         won = True
+instructions(board)
+displayboard(board)
+move(board, player)
 check_win(board, player)
+if check_win(board, "O") == True:
+  print("Player 1 wins!")
+elif check_win(board, "X") == True:
+  print("Player 2 wins!")
