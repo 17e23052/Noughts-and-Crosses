1e524c7187c7bc377643ffa3b008916d8218f8ba
Mathman <70949660+17e23052@users.noreply.github.com>
2021-07-07 16:31:39 +0000

    new 7/7

--- main.py
+++ main.py
@@ -11,25 +11,18 @@ def instructions(board):
   sleep(2)
   print("To place your O or X on the board!")
   sleep(2)
-  print("""These are the positions on the grid:
-   1 │ 2 │ 3
-  ───┼───┼───
-   4 │ 5 │ 6
-  ───┼───┼───
-   7 │ 8 │ 9
-   
-   """)
+  print("These are the positions on the grid:")
   sleep(2)
 def displayboard(board):
-    print(" ", board[0][0], "│", board[0][1], "│", board[0][2])
+    print(" ", "1", "│", "2", "│", "3")
     print(" ───┼───┼───")
-    print(" ", board[1][0], "│", board[1][1], "│", board[1][2])
+    print(" ", "4", "│", "5", "│", "6")
     print(" ───┼───┼───")
-    print(" ", board[2][0], "│", board[2][1], "│", board[2][2])
+    print(" ", "7", "│", "8", "│", "9")
 player = "X"
 def move(board, player):
   print("Where do you want to place your cross?")
-  position = int(input())
+  position = input()
   if position == "1":
     board[0][0] = player
   elif position == "2":
@@ -48,10 +41,18 @@ def move(board, player):
     board[2][1] = player
   elif position == "9":
     board[2][2] = player
+  else:
+    print("Invalid input")
+  print(" ", board[0][0], "│", board[0][1], "│", board[0][2])
+  print(" ───┼───┼───")
+  print(" ", board[1][0], "│", board[1][1], "│", board[1][2])
+  print(" ───┼───┼───")
+  print(" ", board[2][0], "│", board[2][1], "│", board[2][2])
 def check_win(board, player):
     won = False
     if board[0][0] == player and board[0][1] == player and board[0][2] == player:
         won = True
+        return won
     elif board[1][0] == player and board[1][1] == player and board[1][2] == player:
         won = True
     elif board[2][0] == player and board[2][1] == player and board[2][2] == player:
