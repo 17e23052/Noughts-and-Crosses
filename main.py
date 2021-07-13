92b1b24b0181e983c18b3571cd5b4878e7d7a2a5
Mathman <70949660+17e23052@users.noreply.github.com>
2021-07-13 09:44:52 +0000

    .

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ board = [[" ", " ", " "],
 def instructions(board):
   print("Welcome to Noughts and Crosses!")
   sleep(2)
-  print("Player 1 will be noughts, and player 2 will be crosses.")
+  print("Player 1 will be crosses, and player 2 will be noughts.")
   sleep(2)
   print("Enter the number 1 to 9 to choose a location on the grid")
   sleep(2)
@@ -21,38 +21,40 @@ def displayboard(board):
     print(" ───┼───┼───")
     print(" ", "7", "│", "8", "│", "9")
 def move(board, player):
-  print("Where do you want to place your cross?")
-  position = input()
-  if position == "1":
-    board[0][0] = player
-  elif position == "2":
-    board[0][1] = player
-  elif position == "3":
-    board[0][2] = player
-  elif position == "4":
-    board[1][0] = player
-  elif position == "5":
-    board[1][1] = player
-  elif position == "6":
-    board[1][2] = player
-  elif position == "7":
-    board[2][0] = player
-  elif position == "8":
-    board[2][1] = player
-  elif position == "9":
-    board[2][2] = player
-  else:
-    print("Invalid input")
-  print(" ", board[0][0], "│", board[0][1], "│", board[0][2])
-  print(" ───┼───┼───")
-  print(" ", board[1][0], "│", board[1][1], "│", board[1][2])
-  print(" ───┼───┼───")
-  print(" ", board[2][0], "│", board[2][1], "│", board[2][2])
+  turncount = 0
+  while turncount != 9:
+    print("Where do you want to place your cross?")
+    position = input()
+    if position == "1":
+      board[0][0] = player
+    elif position == "2":
+      board[0][1] = player
+    elif position == "3":
+      board[0][2] = player
+    elif position == "4":
+      board[1][0] = player
+    elif position == "5":
+      board[1][1] = player
+    elif position == "6":
+      board[1][2] = player
+    elif position == "7":
+      board[2][0] = player
+    elif position == "8":
+      board[2][1] = player
+    elif position == "9":
+      board[2][2] = player
+    else:
+      print("Invalid input")
+    print(" ", board[0][0], "│", board[0][1], "│", board[0][2])
+    print(" ───┼───┼───")
+    print(" ", board[1][0], "│", board[1][1], "│", board[1][2])
+    print(" ───┼───┼───")
+    print(" ", board[2][0], "│", board[2][1], "│", board[2][2])
+    turncount = turncount + 1
 def check_win(board, player):
     won = False
     if board[0][0] == player and board[0][1] == player and board[0][2] == player:
         won = True
-        return won
     elif board[1][0] == player and board[1][1] == player and board[1][2] == player:
         won = True
     elif board[2][0] == player and board[2][1] == player and board[2][2] == player:
@@ -67,11 +69,12 @@ def check_win(board, player):
         won = True
     elif board[0][2] == player and board[1][2] == player and board[2][2] == player:
         won = True
+    return won
 instructions(board)
 displayboard(board)
 move(board, player)
 check_win(board, player)
-if check_win(board, "O") == True:
+if check_win(board, "X") == True:
   print("Player 1 wins!")
-elif check_win(board, "X") == True:
+elif check_win(board, "O") == True:
   print("Player 2 wins!")
